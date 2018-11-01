8df66e08ca30e9c77c01912a683aee122698b9e6
이두한 <dh.lee@ttb.co.kr>
2018-11-01 17:46:48 +0900

    [mod]front//페이지 이동 모듈 리팩토링, 페이지이동 할때 전달할 객체를 받아서 처리 할수 있게 변경

--- assets/res/www/js/header.js
+++ assets/res/www/js/header.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
         * @author dh.lee@ttb.co.kr
         * @version 0.1
         * @param {string} type 메뉴 타입 으로 sales(영업사원), construction (시공사)로 구분 한다. 
-        * @param {string} title 메뉴 타입 으로 sales(영업사원), construction (시공사)로 구분 한다. 
-        * @param {string} page 헤터 종류로 main 만 달라서 만듬 main 이 아니면 공통으로 간다. 
+        * @param {string} title 타이틀 문자 를 받는다. 
+        * @param {string} page 헤터 종류로 main, menu(홈버튼이 있다.), 그외 가 있다. 헤더가 다 틀려서 만듬
         * @param {function} callback 콜백이 있는경우 헤더 태그를 인자로 넣어서 실행해 준다.  
         * @since 2018.10.22
         * @description 헤더 공통 모듈 
@@ -22,6 +22,29 @@ $(document).ready(function(){
             headerType:null,
             callback:null
         },argParam)
+
+        // 현재 페이지 이름 가져오기
+        var path = location.pathname.split("/").pop();
+
+        // 동일한 페이지 인경우에 대한 처리를 수행한다. 
+        function pathFilter(tagString){
+            //동일한 페이지 인경우 이벤트를 삭제 하고 해당 태그는 선택 클래스를 추가한다. 
+            var $el = $(tagString);
+
+            // console.log($el);
+            // console.log($($el)[0].outerHTML);
+            // console.log(path,$($el).find("a").data("eventPage"));
+
+            if(path === $($el).find("a").data("eventPage")){
+                $($el).find("a").removeAttr("data-event-page");
+                // console.log($($el).find("a"),$($el)[0].outerHTML);
+                return $($el)[0].outerHTML;
+            }else{
+                return tagString;
+            }
+        }
+
+        // 화면 랜더링 설정파일 
         var config = {
             sales:{
                 title:(param.title)? param.title : "영업활동",
@@ -42,7 +65,8 @@ $(document).ready(function(){
                     '<li class="yellow"><a href="#" data-event-page="B_02_51.html">B2C 진도관리</a></li>',
                     '<li class="blue"><a href="#" data-event-page="A_05_02.html">시공승인관리</a></li>',
                     '<li class="setting"><a href="#" data-event-page="L_01_07.html"><span>설정</span></a></li>',
-                ].join(""),
+                ].map(pathFilter)
+                .join(""),
             },
             construction:{
                 title:(param.title)? param.title : "시공",
@@ -51,7 +75,8 @@ $(document).ready(function(){
                     '<li class="green"><a href="#" data-event-page="S_06_11.html">실측관리</a></li>',
                     '<li class="green"><a href="#" data-event-page="C_07_11.html">시공관리</a></li>',
                     '<li class="setting"><a href="#" data-event-page="L_01_07.html"><span>설정</span></a></li>',
-                ].join(""),
+                ].map(pathFilter)
+                .join(""),
             }
         };
         //헤더 태그 랜더링
